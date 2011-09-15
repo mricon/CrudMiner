93d505d3b02df993f6bd58f10b48da31a6618808
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-09-15 15:36:34 -0400

    Add more tests.

---
+++ tests/joomla/1.6/fail/libraries/joomla/version.php
@@ -0,0 +1,16 @@
+class JVersion
+{
+	/** @public string Product */
+	public $PRODUCT	= 'Joomla!';
+	/** @public int Main Release Level */
+	public $RELEASE	= '1.6';
+	/** @public string Development Status */
+	public $DEV_STATUS	= 'Stable';
+	/** @public int Sub Release Level */
+	public $DEV_LEVEL	= '6';
+	/** @public int build Number */
+	public $BUILD		= '';
+	/** @public string Codename */
+	public $CODENAME	= 'Onward';
+	/** @public string Date */
+	public $RELDATE	= '26-Jul-2011';
