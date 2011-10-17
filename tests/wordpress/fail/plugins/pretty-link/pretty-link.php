989c05ef8d096c435310e3d4dd2158d4ec9474f0
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-17 14:40:20 -0400

    Add pretty-link WP plugin.

---
+++ tests/wordpress/fail/plugins/pretty-link/pretty-link.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Pretty Link Lite
+Plugin URI: http://blairwilliams.com/pretty-link
+Description: Shrink, track and share any URL on the Internet from your WordPress website!
+Version: 1.4.56
+Author: Caseproof
+Author URI: http://caseproof.com
+Copyright: 2004-2011, Caseproof, LLC
