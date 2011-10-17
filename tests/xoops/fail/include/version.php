77edcdd8b6c9c281228909eded2b39fc56460094
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-17 15:12:57 -0400

    Add xoops and two Joomla vulns.

---
+++ tests/xoops/fail/include/version.php
@@ -0,0 +1,12 @@
+/**
+ * INCLUDE Licence Header
+ *
+ * It is highly discouraged to use the license file
+ * It will be depreciated and removed
+ */
+@include_once dirname(__FILE__) . '/license.php';
+
+/**
+ *  Define XOOPS version
+ */
+define('XOOPS_VERSION', 'XOOPS 2.5.1a');
