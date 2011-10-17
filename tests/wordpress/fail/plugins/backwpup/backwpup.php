591a2b0c1a31e1c3a7a1eb2cb3698db139425019
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-17 14:28:11 -0400

    Add backwpup wordpress plugin

---
+++ tests/wordpress/fail/plugins/backwpup/backwpup.php
@@ -0,0 +1,10 @@
+<?php
+//Set plugin dirname
+define('BACKWPUP_PLUGIN_BASEDIR', dirname(plugin_basename(__FILE__)));
+define('BACKWPUP_PLUGIN_BASEURL',plugins_url('',__FILE__));
+//Set Plugin Version
+define('BACKWPUP_VERSION', '2.1.4');
+//Set Min Wordpress Version
+define('BACKWPUP_MIN_WORDPRESS_VERSION', '3.1');
+//Set User Capability
+define('BACKWPUP_USER_CAPABILITY', 'export');
