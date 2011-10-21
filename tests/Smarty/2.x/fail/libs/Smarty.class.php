f002c72b8fd2a1341d1f1423ffdce6aaa077be06
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-21 14:32:24 -0400

    Add Smarty.

---
+++ tests/Smarty/2.x/fail/libs/Smarty.class.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * md5 checksum of the string 'Smarty'
+     *
+     * @var string
+     */
+    var $_smarty_md5           = 'f8d698aea36fcbead2b9d5359ffca76f';
+
+    /**
+     * Smarty version number
+     *
+     * @var string
+     */
+    var $_version              = '2.6.22';
+
+    /**
+     * current template inclusion depth
+     *
+     * @var integer
+     */
+    var $_inclusion_depth      = 0;
+?>
---
+++ tests/Smarty/3.x/fail/libs/Smarty.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This is the main Smarty class
+ */
+class Smarty extends Smarty_Internal_Data {
+	/**
+	* constant definitions
+	*/
+    // smarty version
+    const SMARTY_VERSION = 'Smarty-3.0.7';
+  	//define variable scopes
+	const SCOPE_LOCAL = 0;
+	const SCOPE_PARENT = 1;
+	const SCOPE_ROOT = 2;
+	const SCOPE_GLOBAL = 3;
+?>
