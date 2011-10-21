1e696ee514cd6cce5ab987f5b417054017e2109a
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-21 13:37:20 -0400

    Add net2ftp and Lumbroso's formmail.

---
+++ tests/net2ftp/fail/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+// ----------------------------------------------------------------------------------
+// DO NOT CHANGE ANYTHING BELOW THIS LINE
+// ----------------------------------------------------------------------------------
+
+$net2ftp_settings["application_version"] = "0.97a";
+$net2ftp_settings["application_build_nr"] = "45";
+
+// Is this net2ftp.com, or a net2ftp installation elsewhere
+$net2ftp_settings["net2ftpdotcom"] = "no";
+
+// Google Adsense advertisements 
+// Not shown when using HTTPS to avoid warnings on each pageload
+$net2ftp_settings["show_google_ads"] = "no";
+
+?>
