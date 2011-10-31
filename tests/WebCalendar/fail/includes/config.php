f9228c54f322dfc02c8ecb90d9d7a92e0c461ede
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-31 17:03:00 -0400

    Add WebCalendar.

---
+++ tests/WebCalendar/fail/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+  $PROGRAM_VERSION = 'v1.2.0';
+  $PROGRAM_DATE = '14 Aug 2010';
+  $PROGRAM_NAME = 'WebCalendar ' . "$PROGRAM_VERSION ($PROGRAM_DATE)";
+  $PROGRAM_URL = 'http://www.k5n.us/webcalendar.php';
+  $TROUBLE_URL = 'docs/WebCalendar-SysAdmin.html#trouble';
+?>
