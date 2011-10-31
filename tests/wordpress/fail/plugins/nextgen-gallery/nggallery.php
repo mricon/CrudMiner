78ec1093d6704494accd3f1c4c131a24c8a169d9
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-31 14:34:03 -0400

    Add a couple of wordpress plugins.

---
+++ tests/wordpress/fail/plugins/nextgen-gallery/nggallery.php
@@ -0,0 +1,14 @@
+<?php
+// ini_set('display_errors', '1');
+// ini_set('error_reporting', E_ALL);
+if (!class_exists('nggLoader')) {
+class nggLoader {
+	
+	var $version     = '1.8.3';
+	var $dbversion   = '1.8.0';
+	var $minium_WP   = '3.1';
+	var $donators    = 'http://nextgen.boelinger.com/donators.php';
+	var $options     = '';
+	var $manage_page;
+	var $add_PHP5_notice = false;
+?>
---
+++ tests/wordpress/fail/plugins/wp-polls/wp-polls.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP-Polls
+Plugin URI: http://lesterchan.net/portfolio/programming/php/
+Description: Adds an AJAX poll system to your WordPress blog. You can easily include a poll into your WordPress's blog post/page. WP-Polls is extremely customizable via templates and css styles and there are tons of options for you to choose to ensure that WP-Polls runs the way you wanted. It now supports multiple selection of answers.
+Version: 2.60
+Author: Lester 'GaMerZ' Chan
+Author URI: http://lesterchan.net
+*/
