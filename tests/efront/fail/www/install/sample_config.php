69c24646c6d0d8990f85ca4fd0148e604ee139b3
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-10-31 14:03:55 -0400

    Add eFront.

---
+++ tests/efront/fail/www/install/sample_config.php
@@ -0,0 +1,10 @@
+<?php
+/**Software root path*/
+define('G_ROOTPATH', str_replace("\\", "/", dirname(dirname(__FILE__)))."/");
+
+/**Current version*/
+define('G_VERSION_NUM', '3.6.9');
+
+/**Include function files*/
+require_once('globals.php');
+?>
