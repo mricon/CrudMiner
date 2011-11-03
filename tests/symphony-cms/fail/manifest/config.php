68b92d177f71a6aed03d4e3335fb9fb5880fdb5f
Konstantin Ryabitsev <icon@fedoraproject.org>
2011-11-03 14:25:49 -0400

    Add symphony-cms and a couple of WP plugins.

---
+++ tests/symphony-cms/fail/manifest/config.php
@@ -0,0 +1,15 @@
+<?php
+
+        ###### SYMPHONY ######
+        'symphony' => array(
+            'pagination_maximum_rows' => '17',
+            'allow_page_subscription' => '1',
+            'lang' => 'en',
+            'version' => '2.2.3',
+            'pages_table_nest_children' => 'yes',
+            'strict_error_handling' => 'yes',
+            'cookie_prefix' => 'sym-',
+            'session_gc_divisor' => '10',
+        ),
+        ########
+?>
