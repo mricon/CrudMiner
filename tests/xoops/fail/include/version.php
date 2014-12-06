9f3b47286473768c83c0c1872ea1653370215265
Robert Kerr <rkerr@users.sourceforge.net>
2014-12-06 13:52:07 +0000

    Xoops 2.5.7

--- tests/xoops/fail/include/version.php
+++ tests/xoops/fail/include/version.php
@@ -1,12 +1,4 @@
 /**
- * INCLUDE Licence Header
- *
- * It is highly discouraged to use the license file
- * It will be depreciated and removed
- */
-@include_once dirname(__FILE__) . '/license.php';
-
-/**
  *  Define XOOPS version
  */
-define('XOOPS_VERSION', 'XOOPS 2.5.1a');
+define('XOOPS_VERSION', 'XOOPS 2.5.6');
