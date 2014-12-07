67eaf17b92a45a0f07996e53f224afc1306bad43
Robert Kerr <rkerr@users.sourceforge.net>
2014-12-07 18:04:11 +0000

    Add ownCloud

---
+++ tests/owncloud/6.x/fail/version.php
@@ -0,0 +1,6 @@
+<?php 
+$OC_Version = array(6,0,5,1);
+$OC_VersionString = '6.0.5';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_Build = '2014-08-26T10:13:39+00:00';
---
+++ tests/owncloud/7.x/fail/version.php
@@ -0,0 +1,6 @@
+<?php 
+$OC_Version = array(7,0,2,1);
+$OC_VersionString = '7.0.2';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_Build = '2014-08-26T10:19:47+00:00';
